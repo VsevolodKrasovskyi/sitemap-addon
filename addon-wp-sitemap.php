c8c15bb99e4e9d1daddda2a6e32fa1b139fa8aed
VsevolodKrasovskyi <sevakrasovskiy@gmail.com>
2024-05-29 15:51:58 +0300

    1.3

---
+++ addon-wp-sitemap.php
@@ -0,0 +1,99 @@
+<?php
+/*
+Plugin Name: WP Sitemap Addon
+Description: Group pages for your sitemap by simply inserting a simple shortcode [wp_sitemap_group_pages parent="{page_id}"] or [wp_sitemap_parent_no_children]
+Version: 1.2
+Author: Netpeak 
+Author URI: https://netpeak.bg
+*/
+
+
+if ( !class_exists( 'Puc_v4_Factory' ) ) {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/plugin-update-checker-master/plugin-update-checker.php';
+}
+
+
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/VsevolodKrasovskyi/sitemap-addon.git', 
+    __FILE__, 
+    'addon-wp-sitemap'
+);
+
+// Ensure that the parent plugin is active
+if (in_array('wp-sitemap-page/wp-sitemap-page.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+
+    function wpsitemap_custom_grouped_pages($atts) {
+        $atts = shortcode_atts(array(
+            'parent' => 0,
+        ), $atts, 'wp_sitemap_group_pages');
+
+        $parent_id = intval($atts['parent']);
+        
+        if (!$parent_id) {
+            return 'Invalid parent ID';
+        }
+
+        $output = '<ul>' . wpsitemap_get_page_and_children($parent_id) . '</ul>';
+        
+        return $output;
+    }
+
+    function wpsitemap_get_page_and_children($parent_id) {
+        $output = '';
+
+        $parent_page = get_post($parent_id);
+        if ($parent_page) {
+            $output .= '<li><a href="' . get_permalink($parent_page->ID) . '">' . $parent_page->post_title . '</a>';
+
+            $child_pages = get_pages(array(
+                'child_of' => $parent_id,
+                'sort_column' => 'post_title',
+                'sort_order' => 'ASC'
+            ));
+
+            if (!empty($child_pages)) {
+                $output .= '<ul>';
+                foreach ($child_pages as $child_page) {
+                    $output .= wpsitemap_get_page_and_children($child_page->ID);
+                }
+                $output .= '</ul>';
+            }
+
+            $output .= '</li>';
+        }
+
+        return $output;
+    }
+
+    function wpsitemap_parent_no_children() {
+        $pages = get_pages(array(
+            'sort_column' => 'post_title',
+            'sort_order' => 'ASC',
+            'parent' => 0
+        ));
+
+        $parent_no_children_pages = array_filter($pages, function($page) {
+            $children = get_pages(array(
+                'child_of' => $page->ID,
+                'post_status' => 'publish'
+            ));
+            return empty($children);
+        });
+
+        if (empty($parent_no_children_pages)) {
+            return 'No parent pages without children found';
+        }
+
+        $output = '<ul>';
+        foreach ($parent_no_children_pages as $page) {
+            $output .= '<li><a href="' . get_permalink($page->ID) . '">' . $page->post_title . '</a></li>';
+        }
+        $output .= '</ul>';
+
+        return $output;
+    }
+
+    add_shortcode('wp_sitemap_group_pages', 'wpsitemap_custom_grouped_pages');
+    add_shortcode('wp_sitemap_parent_no_children', 'wpsitemap_parent_no_children');
+}
+?>
